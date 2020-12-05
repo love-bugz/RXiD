7b065767df526220d1a3473077118636c8f9538d
Katie Embrey-Farquhar <kembreyfarquhar@gmail.com>
2020-12-05 10:12:11 -0600

    initial commit

---
+++ screens/CaptureScreen.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function CaptureScreen() {
+    return (
+        <View>
+            <Text>Capture Screen</Text>
+        </View>
+    );
+}
---
+++ types.ts
@@ -0,0 +1,21 @@
+export type CaptureStackParamList = {
+    CaptureScreen: undefined;
+};
+
+export type ReminderStackParamList = {
+    ReminderScreen: undefined;
+};
+
+export type HomeTabsParamList = {
+    Capture: undefined; //this is CaptureScreenStack
+    Reminder: undefined; //this is ReminderScreenStack
+};
+
+export type IndexStackParamList = {
+    AppLoading: undefined; //screen
+    Login: undefined; //screen
+    Home: undefined; //Home is a bottom tab navigator
+};
+
+//each tab is a stack
+//make stacks for the components, then render those as tab screens
