a29be6ee01b8c896af33c6a8be4fbfdf5c19f0c2
Katie Embrey-Farquhar <kembreyfarquhar@gmail.com>
2020-12-06 03:37:36 -0600

    adds helper functions

---
+++ utils/asyncStorageFunctions.ts
@@ -0,0 +1,21 @@
+import AsyncStorage from "@react-native-community/async-storage";
+
+export async function setAsyncStorageItems(items: [string, string][]) {
+    try {
+        items.forEach(async item => {
+            await AsyncStorage.setItem(item[0], item[1]);
+        });
+    } catch (err) {
+        throw err;
+    }
+}
+
+export async function removeAsyncStorageItems(items: string[]) {
+    try {
+        items.forEach(async item => {
+            await AsyncStorage.removeItem(item);
+        });
+    } catch (err) {
+        throw err;
+    }
+}
